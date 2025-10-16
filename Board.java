e3cf98a52aa929de88d8af4e4c07d3e8b7382eaf
aveW618 <wangave27@7hills.org>
2025-10-15 21:21:51 -0400

    loops

--- Board.java
+++ Board.java
@@ -6,7 +6,17 @@ public class Board {
 	
 	//toString method to print out both boards
 	public String toString() {
+		//initialize variable result
+		String result = "My Board: \n";
 		
+		//print player1's board
+		//for loop for the rows of the board
+		for (int r=0;r<10;r++) {
+			//for loop for the columns of the board
+			for (int c = 0; c <10; c++) {
+				//make all rows and columns dashes (array of dashes)
+				result += player1Board[r][c] + "-";
+			
 		
 		
 		
