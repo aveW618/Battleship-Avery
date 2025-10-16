f9844639496abb8844494c2d87975c7e8c666b85
aveW618 <wangave27@7hills.org>
2025-10-15 23:35:03 -0400

    fill the board with dashes

--- Board.java
+++ Board.java
@@ -4,6 +4,17 @@ public class Board {
 	char [][] player1Board = new char[10][10];
 	char[][] player2Board = new char [10][10];
 	
+	//constructor
+	public Board() {
+		//loops through rows and columns
+		for (int r = 0; r<10; r++) {
+			for (int c = 0; c<10; c++) {
+				//fills the boards with dashes
+				player1Board[r][c]="-";
+				player2Board[r][c] = "-";
+			}
+		}
+	
 	//toString method to print out both boards
 	public String toString() {
 		//initialize variable result
