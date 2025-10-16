2145e08d11add840df7492b974e3ebcf8a7183a3
aveW618 <wangave27@7hills.org>
2025-10-15 20:46:08 -0400

    first commit

---
+++ Board.java
@@ -0,0 +1,12 @@
+//create a class called Board
+public class Board {
+	//creates two 10 by 10 boards use the char data type
+	char [][] player1Board = new char[10][10];
+	char[][] player2Board = new char [10][10];
+	
+	//toString method to print out both boards
+	public String toString() {
+		
+		
+		
+		
