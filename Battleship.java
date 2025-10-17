ef880efae19a8896cfdc7f736fa39649627b6b03
aveW618 <wangave27@7hills.org>
2025-10-17 08:58:57 -0400

    first rules

---
+++ Battleship.java
@@ -0,0 +1,13 @@
+import java.util Sccanner
+public class Battleship {
+	
+	System.out.println("Welcome to Battleship!");
+	System.out.print("Type R to access the rules of this game. Type P to play: ");
+	
+	Scanner userInput = new Scanner(System.in)
+	
+	char play = scan.nextChar;
+	
+	if (play = R) {
+		System.out.println(
+	
