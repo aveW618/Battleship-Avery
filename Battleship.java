9eb89a4859fc80488f17dd161cfd9775cbc07246
aveW618 <wangave27@7hills.org>
2025-10-17 09:28:15 -0400

    begin placeShips method - 2nd class commit

--- Battleship.java
+++ Battleship.java
@@ -8,6 +8,32 @@ public class Battleship {
 	
 	char play = scan.nextChar;
 	
-	if (play = R) {
-		System.out.println(
+	if (play = 'R') {
+		System.out.println("You will start with a board with two grids.");
+		System.out.println("One grid is where you will arrange your ships and record your opponent's shots. The other board is where you will record your own shots.");
+		System.out.println("You will have 5 ships: a carrier (Length: 5), a battleship (Length: 4), a destroyer (Length: 3), a submarine (Length: 3), and a patrol boat (Length: 2)");
+		System.out.println("You must arrange the ships horizontally or vertically without any overlapping");
+		System.out.println("Your goal is to shoot all of your opponent's ship.");
+		System.out.println("Each successful hit will be marked on your shot grid with the character 'h'. Each miss will be marked with the character 'm'.");
+		System.out.println("Each of your opponent's hits will be marked on your ship grid with the character 'h' for hits and 'm' for misses.");
+		System.out.println("Whoever sinks all the opponent's ships first wins");
+		System.out.println("Type B to begin!");
+		Scanner ruleInput = new Scanner(System.in)
+		
+		char start = scan.nextChar;
+		if (start = 'B') {
+			char play = run ....... //finish this after runner is made
+		}
+	}
+			
+	else if (play = 'P') {
+		char play = run...... //finish this after runner is made
+	}
+	
+	else {
+		System.out.println("Error. Try again");
+		System.out.print("Type R to access the rules of this game. Type P to play: ");
+	}
+	
+			
 	
