dffcb8d8c418cd762575a06d06c662fbe201c180
aveW618 <wangave27@7hills.org>
2025-10-16 20:51:40 -0400

    syntax errors, comments

--- Board.java
+++ Board.java
@@ -1,50 +1,52 @@
 //create a class called Board
 public class Board {
 	//creates two 10 by 10 boards use the char data type
-	char [][] player1Board = new char[10][10];
+	//from Runestone Academy chapter 4.3
+	char[][] player1Board = new char[10][10];
 	char[][] player2Board = new char [10][10];
 	
 	//constructor
 	public Board() {
 		//loops through rows and columns
+		//runestone chapter 4.4
 		for (int r = 0; r<10; r++) {
 			for (int c = 0; c<10; c++) {
-				//fills the boards with dashes
-				player1Board[r][c]="-";
-				player2Board[r][c] = "-";
+				//fills the boards with dashes separated by spaces
+				player1Board[r][c]= '-';
+				player2Board[r][c] = '- ';
 			}
 		}
+	}
 	
 	//toString method to print out both boards
 	public String toString() {
-		//initialize variable result
-		String result = "My Board: \n";
 		
 		//print player1's board
+		System.out.println("Player 1 Board: ");
 		//for loop for the rows of the board
 		for (int r=0;r<10;r++) {
 			//for loop for the columns of the board
 			for (int c = 0; c <10; c++) {
-				//prints out all the rows and columns separated by a space
-				result += player1Board[r][c] + " ";
+				//prints out all the rows and columns
+				System.out.print(player1Board[r][c]);
 			}
 			//new line for each row
-			result += "\n";
+			System.out.println();
 		}
 		
 		//print player2's board
+		System.out.println("Player 2 Board: ");
 		//for loop for the rows of the board
 		for (int r=0;r<10;r++) {
 			//for loop for the columns of the board
+			//runestone chapter 4.4
 			for (int c = 0; c <10; c++) {
-				//prints out all the rows and columns separated by a space
-				result += player2Board[r][c] + " ";
+				//prints out all the rows and columns
+				System.out.print(player2Board[r][c]);
 			}
 			//new line for each row
-			result += "\n";
+			System.out.println();
 		}
-		
-		return result;
 	}
 }
 			
